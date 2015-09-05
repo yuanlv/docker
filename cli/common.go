c03e0465c13678db62749f11c2a6fe838c74540e
snowfox <yuanlv@126.com>
2015-09-05 19:56:47 +0800

    添加中文注释
    提供中英文注释对照

--- cli/common.go
+++ cli/common.go
@@ -6,6 +6,7 @@ import (
 )
 
 // CommonFlags represents flags that are common to both the client and the daemon.
+// CommonFlags 结构包含对于docker 客户端和守护进程通用的标记信息
 type CommonFlags struct {
 	FlagSet   *flag.FlagSet
 	PostParse func()
