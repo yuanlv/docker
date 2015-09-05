f9f2dab4d043f7e3aa542da2b1e097beaf22e020
yuanlv <yuanlv@126.com>
2015-09-05 21:10:45 +0800

    add comments for client.go

--- cli/client.go
+++ cli/client.go
@@ -3,6 +3,7 @@ package cli
 import flag "github.com/docker/docker/pkg/mflag"
 
 // ClientFlags represents flags for the docker client.
+// ClientFlags 结构体代表docker client的标记信息
 type ClientFlags struct {
 	FlagSet   *flag.FlagSet
 	Common    *CommonFlags
